2fa87a36795f72947d9af9747edff8e5925f660c
nikitab09 <nikitaborgohain06@gmail.com>
2026-03-31 23:49:49 +0530

    add 2 more solutions

---
+++ longest_palindrome.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        if not s:
+            return ""
+
+        start = 0
+        end = 0
+
+        # Expand around center function
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return right - left - 1
+
+        for i in range(len(s)):
+            # Odd length palindrome
+            len1 = expand(i, i)
+
+            # Even length palindrome
+            len2 = expand(i, i + 1)
+
+            max_len = max(len1, len2)
+
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+
+        return s[start:end + 1]
